efbc5a1f0f8ca00a9060dc99e50e7b9515d4bbfb
tarek <tarek.ouachani01@gmail.com>
2023-01-13 21:56:48 +0000

    qr with the chart.googleApi

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,15 @@
 // methode 1 : 
 // Using qrcode.js library by adding the cdn 
-var button = document.getElementById("generate-button");
+var button    = document.getElementById("generate-button");
+var inputText = document.getElementById("text");
+var showQr    = document.getElementById("qrcode");
+
 button.addEventListener("click", function() {
-    var inputText = document.getElementById("text").value;
-    var qrcode = new QRCode(document.getElementById("qrcode"), {
-        text: inputText,
+    var qrcode = new QRCode(showQr, {
+        text: inputText.value,
         width: 400,
         height: 400,
-        colorDark : "#000000",
+        colorDark : "#4949FF",
         colorLight : "#ffffff",
         correctLevel : QRCode.CorrectLevel.H
     });
@@ -17,3 +19,7 @@ button.addEventListener("click", function() {
 // using googlechartApi 
 // cht =qr      chs=<width>x<height>        chl=data
 
+// button.addEventListener("click",()=>{
+//     let src = `https://chart.googleapis.com/chart?chs=500x500&cht=qr&chl=${inputText.value}`;
+//     showQr.innerHTML += `<img src="${src}">`;  
+// })
